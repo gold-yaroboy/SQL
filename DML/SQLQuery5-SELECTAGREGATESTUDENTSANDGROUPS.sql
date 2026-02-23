04382da7b26215b7506e83a4082a9940051a59a7
ilyav <ilyav@ILIA>
2026-02-24 01:00:24 +0500

    HW

--- DML/SQLQuery5-SELECTAGREGATESTUDENTSANDGROUPS.sql
+++ DML/SQLQuery5-SELECTAGREGATESTUDENTSANDGROUPS.sql
@@ -1,25 +1,23 @@
 USE PV_521_Import;
 
 SELECT
-		direction_name				AS	N'Íàïðàâëåíèå îáó÷åíèÿ'
-		,COUNT(DISTINCT group_id)	AS	N'Êîëè÷åñòâî ãðóïï'
-		,COUNT(stud_id)				AS	N'Êîëè÷åñòâî ñòóäåíòîâ'
-FROM	Students,Groups,Directions
-WHERE	Students.[group]			=	Groups.group_id
-AND		Groups.direction			=	Directions.direction_id
+			direction_name				AS	N'Направление обучения'
+			,COUNT(DISTINCT group_id)	AS	N'Количество групп'
+			,COUNT(stud_id)				AS	N'Количество студентов'
+FROM		Students,Groups,Directions
+WHERE		Students.[group]			=	Groups.group_id
+AND			Groups.direction			=	Directions.direction_id
 GROUP BY	direction_name
 ;
 
 SELECT
-		direction_name		AS	N'Íàïðàâëåíèå îáó÷åíèÿ'
-		--,COUNT(stud_id)		AS	N'Êîëè÷åñòâî ñóäåíòîâ'
-		--,COUNT(DISTINCT group_id)	AS	N'Êîëè÷åñòâî ãðóïï'
-		,(SELECT COUNT(group_id) FROM Groups WHERE direction=direction_id)	AS N'Êîëè÷åñòâî ãðóïï'
-		,(
-			SELECT COUNT(stud_id) 
-			FROM Students,Groups--,Directions 
-			WHERE [group]=group_id 
-			AND direction=direction_id
-		 )	AS	N'Êîëè÷åñòâî ñòóäåíòîâ'
-FROM	Directions
+			direction_name				AS	N'Направление обучения'
+			,(SELECT COUNT(group_id)	FROM Groups WHERE direction=direction_id)	AS N'Количество групп'
+			,(
+				SELECT COUNT(stud_id) 
+				FROM Students,Groups
+				WHERE [group] = group_id 
+				AND direction = direction_id
+			 )	AS	N'Количество студентов'
+FROM		Directions
 ;
